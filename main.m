841940c2a19d2faf83483396f5b97f09be7345ba
Pavlos Paris Giakoumakis <parisgiakoum@gmail.com>
2020-01-21 15:37:42 +0100

    GMMs fitted to data
    Fitted a GMM to each subject's data and test if the distributions describe the data using Kolmogrov-Smirnov tests

--- main.m
+++ main.m
@@ -13,6 +13,30 @@ database = clearDb(database);
 
 [X, Dt,meanF, len, phi] = computeAllDesiredVariables(force, time, x_coord, y_coord);
 
+%% GM model
+for i=1:size(X,3)   %1-215
+    temp = X(:,1:3,i);
+
+    GMModel{i} = fitgmdist (temp(any(~isnan(temp), 2), :), 5, 'SharedCovariance',true);
+  
+    Y = random(GMModel{i},415);
+
+    T = X(:,1,i);
+    F = X(:,2,i);
+    L = X(:,3,i);
+    TSim=Y(:,1);             
+    FSim=Y(:,2);
+    LSim=Y(:,3);
+
+    h1(i)=kstest2(T,TSim);
+    h2(i)=kstest2(F,FSim);
+    h3(i)=kstest2(L,LSim);
+end
+
+h=[h1; h2; h3];
+% sum(h==1,'all')
+clear h1 h2 h3 T F L TSim FSim LSim
+
 %% First plots of data
 %% Plots
 % plot force-time measurements of a subject (j) in one plot
