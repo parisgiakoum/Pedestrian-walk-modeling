dd0d32fffd19c6401504368f5123a3baa7559f7e
Pavlos Paris Giakoumakis <parisgiakoum@gmail.com>
2020-01-21 15:41:43 +0100

    Plot GMM and data pdf's
    Ploted data pdf's along with the fitted gmm's for each variable to visualise the fitting

--- main.m
+++ main.m
@@ -37,6 +37,39 @@ h=[h1; h2; h3];
 % sum(h==1,'all')
 clear h1 h2 h3 T F L TSim FSim LSim
 
+%% Try plotting gmm and data
+%% Time
+i=1;
+
+tempgm = gmdistribution(GMModel{i}.mu(:,1), GMModel{i}.Sigma(1), GMModel{i}.PComponents);
+figure;
+temp = X(:,1,i);
+histogram (temp(~isnan(temp)), 'BinWidth', 0.02, 'BinLimits',[0,1.6], 'normalization' , 'pdf' );
+xgrid = linspace(0,1.6,1000)';
+hold on; plot(xgrid,pdf(tempgm,xgrid),'r-'); hold off
+
+%% Force
+i=1;
+
+tempgm = gmdistribution(GMModel{i}.mu(:,2), GMModel{i}.Sigma(2,2), GMModel{i}.PComponents);
+figure;
+temp = X(:,2,i);
+histogram (temp(~isnan(temp)), 'BinWidth', 2, 'BinLimits',[0 ,120], 'normalization' , 'pdf' );
+xgrid = linspace(0,120,1000)';
+hold on; plot(xgrid,pdf(tempgm,xgrid),'r-'); hold off
+
+%% Length
+i=5;
+
+tempgm = gmdistribution(GMModel{i}.mu(:,3), GMModel{i}.Sigma(3,3), GMModel{i}.PComponents);
+figure;
+temp = X(:,3,i);
+histogram (temp(~isnan(temp)), 'BinWidth', 0.03, 'BinLimits',[0,1.7], 'normalization' , 'pdf' );
+xgrid = linspace(0,1.7,1000)';
+hold on; plot(xgrid,pdf(tempgm,xgrid),'r-'); hold off
+
+
+
 %% First plots of data
 %% Plots
 % plot force-time measurements of a subject (j) in one plot
